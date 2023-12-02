b8a80a2ad96f0edd94cc6bf004c3868a31e1c17a
softech <softech@DESKTOP-QCJLS92.lan>
2023-12-01 21:48:12 -0500

    Carga inicial

---
+++ src/main/java/uisrael/poo/lab1/HolaMundo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package uisrael.poo.lab1;
+
+/**
+ *
+ * @author softech
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+}
